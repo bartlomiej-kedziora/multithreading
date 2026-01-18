96466d70b3d0014b7fcaf99969c674d45e78d4c5
Bartlomiej Kedziora <bartlomiej.kedziora@vp.pl>
2026-01-18 20:44:23 +0100

    EKEDMT-9: pr4: Daemon threads project

--- src/main/java/pl/eked/project/block01/pr04/DaemonThreads.java
+++ src/main/java/pl/eked/project/block01/pr04/DaemonThreads.java
@@ -19,7 +19,25 @@ package pl.eked.project.block01.pr04;
  */
 public class DaemonThreads {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new MyTask());
+        thread.setDaemon(true);
+        thread.start();
+        //thread.join(); -> this will block main thread even if set daemon
+    }
 
+    private static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    System.out.println("Message executed every 1s");
+                    Thread.sleep(1_000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
